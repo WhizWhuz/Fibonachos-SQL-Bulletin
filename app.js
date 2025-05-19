7fa75e4c155426fc64d038f7cbac60407df3c7a6
WhizWhuz <jesper_andren94@hotmail.com>
2025-05-19 16:33:51 +0700

    initial commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const postRoutes = require("./routes/posts");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+//Routes
+
+module.exports = app;
