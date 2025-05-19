f635ce8ff8bc6a05e0098ebe517cd2678314bfcb
WhizWhuz <jesper_andren94@hotmail.com>
2025-05-19 16:48:20 +0700

    minor changes

--- app.js
+++ app.js
@@ -1,8 +1,5 @@
 const express = require("express");
 const cors = require("cors");
-require("dotenv").config();
-
-const postRoutes = require("./routes/posts");
 
 const app = express();
 
--- server.js
+++ server.js
@@ -1,5 +1,9 @@
-const app = require('./app');
+const app = require("./app");
 const PORT = process.env.PORT || 3000;
+
+const dotenv = require("dotenv");
+
+dotenv.config({ path: "./config.env" });
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
