277befdcce2bacf92d040c13feb4f5424d33157b
WhizWhuz <jesper_andren94@hotmail.com>
2025-05-20 14:05:40 +0700

    reorganizing

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-const app = require("./app");
-const PORT = process.env.PORT || 3000;
-
 const dotenv = require("dotenv");
+const app = require("./app");
 
 dotenv.config({ path: "./config.env" });
+
+const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
