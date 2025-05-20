fe5ad891a0b4958ffefe5a62e7bb65b2c842449c
axile24 <amokrane.n@icloud.com>
2025-05-20 10:00:56 +0100

    Lagt till messageRoutes

--- app.js
+++ app.js
@@ -2,13 +2,15 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const userRoutes = require("./routes/userRoutes");
-
 const channelRoutes = require("./routes/channelRoutes");
+const messageRoutes = require("./routes/messageRoutes");
+
 app.use(cors());
 app.use(express.json());
 
 //Routes
 app.use("/channels", channelRoutes);
 app.use("/users", userRoutes);
+app.use("/", messageRoutes);
 
 module.exports = app;
