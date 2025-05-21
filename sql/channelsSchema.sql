215aa5eb41ff8d59cf23c18cdc84b22858abf170
WhizWhuz <jesper_andren94@hotmail.com>
2025-05-21 13:27:01 +0700

    made changes to sql to be inline with team

--- sql/channelsSchema.sql
+++ sql/channelsSchema.sql
@@ -1,9 +1,9 @@
 CREATE TABLE channels (
-    id SERIAL PRIMARY KEY,
+    channel_id SERIAL PRIMARY KEY,
     name TEXT NOT NULL, 
     description TEXT,
     created_at TIMESTAMP DEFAULT NOW(),
     owner_id INTEGER NOT NULL,
 
-    FOREIGN KEY (owner_id) REFERENCES user(id) ON DELETE CASCADE
+    FOREIGN KEY (owner_id) REFERENCES users(user_id) ON DELETE CASCADE
 );
